0bf5fa1a8f8ff683d18ae5ac6d85289416280b6b
cayean <collinayala@gmail.com>
2022-11-29 21:31:51 -0600

    refactored conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,41 +1,6 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-  // const [categories] = useState([
-  //   {
-  //     name: 'about',
-  //     description: 'blah'
-  //   },
-  //   {
-  //     name: 'portfolio',
-  //     description: 'blah 2'
-  //   },
-  //   {
-  //     name: 'contact',
-  //     description: 'how to contact me'
-  //   },
-  //   {
-  //     name: 'resume',
-  //     description: 'just a resume'
-  //   }
-  // ])
-
-  // const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
-  // add contact form here
-
-  return (
-    <div>
-      <Header>
-      </Header>
-    <main>
-      
-    </main>
-    <Footer />
-    </div>
-  )
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
