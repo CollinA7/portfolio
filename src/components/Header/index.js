c2cb507ec5dcc5289d85c871fa65c5d1b8623fc7
cayean <collinayala@gmail.com>
2022-12-21 21:01:57 -0600

    formatted code

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 export default function Header() {
-    return (
-        <header>
-            <h1>Collin's portfolio</h1>
-        </header>
-    )
+  return (
+    <header>
+      <h1>Collin's portfolio</h1>
+    </header>
+  );
 }
