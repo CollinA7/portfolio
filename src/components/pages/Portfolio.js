cdf4e63472b92a0b8cc7a18f66f2af8795b5969c
cayean <collinayala@gmail.com>
2022-12-21 22:08:40 -0600

    added pathfinder to portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,25 +1,16 @@
-import React from 'react';
+import React from "react";
 
 export default function Portfolio() {
-
   return (
-    <div className='container'>
+    <div className="container">
       <h2>Portfolio</h2>
-      <div className='row'>
-          <div className='col card fyg'>
-            Find your game
-          </div>
-          <div className='col card niche'>
-            Niche
-          </div>
+      <div className="row">
+        <div className="col card fyg">Find your game</div>
+        <div className="col card niche">Niche</div>
       </div>
-      <div className='row'>
-        <div className='col card'>
-            Placeholder
-          </div>
-          <div className='col card'>
-            Placeholder
-          </div>
+      <div className="row">
+        <div className="col card path-finder"></div>
+        <div className="col card">Placeholder</div>
       </div>
     </div>
   );
