1035a52adefd38861ea36648e9dcb945c8566199
cayean <collinayala@gmail.com>
2022-12-23 01:16:42 -0600

    added function to display projects

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -6,42 +6,39 @@ import Col from "react-bootstrap/Col";
 export default function Portfolio() {
   const projectData = [
     {
-      name: "find-your-game",
-      desription: "blah blah",
-      link: "http://blahblah",
+      name: "Find your Game",
+      desription: "Description",
+      gitHublink: "https://github.com/CollinA7/portfolio-2",
+      deployedLink: "https://dlope0831.github.io/find-your-game/",
+      image: "./assets/images/fyg.jpg",
     },
   ];
 
+  const projectMap = projectData.map((data) => (
+    <Col xs={6} className=" card flip-card">
+      <div className="flip-card-inner">
+        <div className="flip-card-front fyg" />
+        <div className="flip-card-back">
+          <h3>{data.name}</h3>
+          <p>{data.desription}</p>
+          <p>also description</p>
+          <a href={data.gitHublink} rel="noreferrer" target="_blank">
+            Github
+          </a>
+          <br />
+          <a href={data.deployedLink} rel="noreferrer" target="_blank">
+            Deployed app
+          </a>
+        </div>
+      </div>
+    </Col>
+  ));
+
+  console.log(projectMap);
   return (
     <Container>
       <h2>Portfolio</h2>
-      <Row>
-        <Col xs={6} className=" card flip-card">
-          <div className="flip-card-inner">
-            <div className="flip-card-front fyg" />
-            <div className="flip-card-back">
-              <h3>Find your Game</h3>
-              <p>game description</p>
-              <p>also description</p>
-              <a
-                href="https://github.com/CollinA7/portfolio-2"
-                rel="noreferrer"
-                target="_blank"
-              >
-                Github
-              </a>
-              <br />
-              <a
-                href="https://dlope0831.github.io/find-your-game/"
-                rel="noreferrer"
-                target="_blank"
-              >
-                Deployed app
-              </a>
-            </div>
-          </div>
-        </Col>
-      </Row>
+      <Row>{projectMap}</Row>
     </Container>
   );
 }
